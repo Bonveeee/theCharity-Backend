7cb76798b1b971606485367f8c1cf3377d9776db
bonveeee <bonochieng@gmail.com>
2022-09-09 03:26:58 +0300

    [added]delete && patch requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -57,5 +57,72 @@ post "/donations" do
  donations.to_json
  end
 
+#delete requests
+delete '/donors/:id' do
+  # find the donor using the ID
+  donor = Donor.find(params[:id])
+  # delete the donor
+  donor.destroy
+  # send a response with the deleted donor as JSON
+  donor.to_json
+end
+
+delete '/campaigns/:id' do
+  # find the campaign using the ID
+  campaign = Campaign.find(params[:id])
+  # delete the campaign
+  campaign.destroy
+  # send a response with the deleted campaignas JSON
+  campaign.to_json
+end
+
+delete '/donations/:id' do
+  # find the donation using the ID
+  donation = Donation.find(params[:id])
+  # delete the donation
+  review.destroy
+  # send a response with the deleted donation as JSON
+  donation.to_json
+end
+
+#these are patch requests
+
+patch '/donors/:id' do
+  donor = Donor.find(params[:id])
+  donor.update(
+    name: params[:name],
+    email: params[:email],
+   description: params[:phone],
+    address: params[:address],
+    country: params[:country]
+  )
+  donor.to_json
+end
+
+
+patch '/donations/:id' do
+  donation = Donation.find(params[:id])
+  donation.update(
+    name: params[:name],
+   date: params[:date],
+  description: params[:description],
+   donor_id: params[:donor_id],
+  )
+  donation.to_json
+end
+
+
+patch '/campaigns/:id' do
+  campaign = Campaign.find(params[:id])
+  campaign.update(
+    name: params[:name],
+   location: params[:location],
+   description: params[:description],
+   amount: params[:amount],
+   duration: params[:duration],
+   donation_id: params[:donation_id]
+  )
+  campaign.to_json
+end
 
 end
